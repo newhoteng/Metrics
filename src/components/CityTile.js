47ef94a1ecbb6d715e18cacd3eb34d7eeebc7ef8
Harriet Oteng <harriet.oteng@gmail.com>
2023-09-26 22:41:09 +0000

    Add air quality description to CityTile component

--- src/components/CityTile.js
+++ src/components/CityTile.js
@@ -1,19 +1,12 @@
 export default function CityTile({ city }) {
+  let airQualityDesc = `aqi: ${city.aqi} - ${city.textValue}`
   return (
     <Link to={`/${city.name}`} key={city.id} className={styles.countryLink}>
       <BsArrowRightCircle className={styles.arrowCircle} />
       <BsWind className={styles.airIcon} style={{ color: `${city.colorIndicator}` }} />
       <div className={styles.linkText}>
         <p>{city.name.toUpperCase()}</p>
-        <p>
-          aqi:
-          {' '}
-          {city.aqi}
-          {' '}
-          -
-          {' '}
-          {city.textValue}
-        </p>
+        <p>{airQualityDesc}</p>
       </div>
     </Link>
   );
