2de57864d849153f6354454ccb10efcf400f9b04
Harriet Oteng <harriet.oteng@gmail.com>
2023-09-28 10:31:19 +0000

    Add air quality data container

---
+++ src/components/AirQDataContainer.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getCurrentAQIs } from '../redux/current/currentSlice';
+import CityList from './CityList';
+
+export default function AirQDataContainer() {
+  const { currentAQIs, isLoading, error } = useSelector((store) => store.currentAQIs);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!currentAQIs.length) {
+      dispatch(getCurrentAQIs());
+    }
+  }, [dispatch, currentAQIs]);
+
+  return (
+    <>
+      {/* display error message if data does not get fetched */}
+      {error && (
+        <div style={{ padding: '3rem' }}>
+          <h1>Something went wrong, please reload the page</h1>
+        </div>
+      )}
+      {/* display 'Loading' or data when loading or fetched respectively */}
+      {isLoading ? <h1>Loading...</h1> : (
+        <CityList cities={currentAQIs} />
+      )}
+    </>
+  )
+  
+}
